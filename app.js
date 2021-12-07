7f400faae4cc101fa4af65a200ce61846d3e7233
Drake Graham <dt_graham@yahoo.com>
2021-12-07 16:52:26 -0700

    tuesday-package

---
+++ app.js
@@ -0,0 +1,83 @@
+const packages = [
+    { heavy: true, priority: false, fragile: false, to: 'Harrington', trackingNumber: '1324kjs' },
+
+    { heavy: false, priority: true, fragile: true, to: 'Mark', trackingNumber: '1325sdk' },
+
+    { heavy: true, priority: false, fragile: true, to: 'Mick', trackingNumber: 'jffd147' },
+
+    { heavy: false, priority: false, fragile: false, to: 'Jake', trackingNumber: 'acdc145' },
+
+    { heavy: true, priority: true, fragile: true, to: 'Brittany' },
+
+    { heavy: false, priority: true, fragile: false, to: 'Jonesy', trackingNumber: '8081baz' },
+
+    { heavy: true, priority: false, fragile: true, to: 'Justin', trackingNumber: 'suz2367' }
+]
+function draw() {
+    let template = ''
+    for (let i = 0; i < packages.length; i++) {
+        let package = packages[i]
+        template += `
+        <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h6>Package shipped to: ${package.to}</h6>
+                        <h6>Heavy: ${package.heavy}</h6>
+                        <h6>Priority: ${package.priority}</h6>
+                        <h6>Fragile: ${package.fragile}
+                        <h6>Tracking Number: ${package.trackingNumber}
+                    </div>
+                </div>
+            </div>
+        `
+    }
+    document.getElementById('packages').innerHTML = template
+}
+function filterHeavy() {
+    let heavyPackages = packages.filter(p => p.heavy)
+    let template = ''
+    for (let i = 0; i < heavyPackages.length; i++) {
+        const package = heavyPackages[i];
+        template += `
+        <div class="col-md-3">
+        <div class="card">
+        <div class="card-body">
+        <h6>Package shipped to: ${package.to}</h6>
+        <h6>Heavy: ${package.heavy}</h6>
+        <h6>Priority: ${package.priority}</h6>
+        <h6>Fragile: ${package.fragile}
+        <h6>Tracking Number: ${package.trackingNumber}
+        </div>
+        </div>
+        </div>
+        `
+    }
+    document.getElementById('packages').innerHTML = template
+
+
+}
+function filterTracking() {
+    let trackingNumber = packages.filter(p => p.trackingNumber)
+    let template = ''
+    for (let i = 0; i < trackingNumber.length; i++) {
+        const package = trackingNumber[i];
+        template += `
+        <div class="col-md-3">
+        <div class="card">
+        <div class="card-body">
+        <h6>Package shipped to: ${package.to}</h6>
+        <h6>Heavy: ${package.heavy}</h6>
+        <h6>Priority: ${package.priority}</h6>
+        <h6>Fragile: ${package.fragile}
+        <h6>Tracking Number: ${package.trackingNumber}
+        </div>
+        </div>
+        </div>
+        `
+    }
+    document.getElementById('packages').innerHTML = template
+}
+
+
+
+draw()
